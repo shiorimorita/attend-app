4f8186b68aa55cdf406a37f02f1d0a00c6860d87
Shiori Morita <afipito456@gmail.com>
2026-01-27 13:34:20 +0900

    修正

--- src/tests/Feature/Admin/AdminStaffListTest.php
+++ src/tests/Feature/Admin/AdminStaffListTest.php
@@ -1,9 +1,8 @@
 <?php
 
-namespace Tests\Feature;
+namespace Tests\Feature\Admin;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\Models\User;
 use App\Models\Attendance;
